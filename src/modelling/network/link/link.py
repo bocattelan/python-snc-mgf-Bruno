25fc4bf1a52dd0e889f3b701efceae9a95f28c22
bocattelan <bocattelan@gmail.com>
2018-12-24 18:06:18 +0100

    A link here is used both in network graphs and service graph. It has a service curve if it is a link (network graph). If a None is its service, then it is a turn (service graph)

--- src/modelling/network/link/link.py
+++ src/modelling/network/link/link.py
@@ -1,11 +1,16 @@
+from modelling.exceptions.modelling_exception import LinkError
 from modelling.network.server.server import Server
+from nc_service.service import Service
 
 
 class Link(object):
 
-    def __init__(self, src: Server, dst: Server):
+    def __init__(self, src: Server, dst: Server, service=None):
         self.__src = src
         self.__dst = dst
+        if not(service is None) and not(issubclass(service.__class__, Service)):
+            raise LinkError(src, dst, "Invalid service for network graph link:" + service.__str__())
+        self.__service = service
 
     @property
     def src(self) -> Server:
@@ -14,6 +19,13 @@ class Link(object):
     @property
     def dst(self) -> Server:
         return self.__dst
+
+    def is_turn(self) -> bool:
+        return self.__service is None
+
+    @property
+    def service(self):
+        return self.__service
 
     def __str__(self):
         return "[link:" + self.__src.alias + ", " + self.__dst.alias + "]"
